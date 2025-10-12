2023f98733f862930a4616d3b4b88a3df6da6675
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-10-12 09:27:41 +0200

    chore: setup Django project structure with DRF and apps

--- studyhub/settings.py
+++ studyhub/settings.py
@@ -37,6 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',
+    'rest_framework_simplejwt',
+
+    # Local apps
+    'users',
+    'courses',
+    'assignments',
+    'submissions',
+    'resources',
+    'announcements',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +132,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+}
