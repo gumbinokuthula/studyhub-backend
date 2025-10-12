b44198a017b1f32e662f38e18125b929291a7553
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-10-12 18:27:07 +0200

    studyhub settings.py Added

--- studyhub/settings.py
+++ studyhub/settings.py
@@ -142,4 +142,14 @@ REST_FRAMEWORK = {
     ),
 }
 
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = 'users.User'
+
+# Django REST Framework Configuration
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
--- users/models.py
+++ users/models.py
@@ -1,15 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
-    ROLE_CHOICES = (
-        ('teacher', 'Teacher'),
+class User(AbstractUser):
+    ROLE_CHOICES = [
         ('student', 'Student'),
-    )
-
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-    bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+        ('instructor', 'Instructor'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
 
     def __str__(self):
-        return f"{self.username} ({self.role})"
+        return self.username
