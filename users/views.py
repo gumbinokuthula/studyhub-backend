cc4d003e383b02cfbc8d67eefec23ec8f1f09823
Nokuthula Gumbi <nokuthulagumbi737@gmail.com>
2025-10-14 20:27:32 +0200

    Initial setup complete: project structure, users app with registration and login

--- users/views.py
+++ users/views.py
@@ -1,9 +1,22 @@
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
-from .models import User
+from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .serializers import RegisterSerializer, UserSerializer
+
+User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        data['role'] = self.user.role
+        return data
+
+class LoginView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
